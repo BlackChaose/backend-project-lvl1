21a74f0496a3ea99857a4784d8f919db9e94adfb
nik <nik@0x4e494b.ru>
2019-10-27 01:41:54 +0300

    save

---
+++ src/bin/brain-calc.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import { whatsName, question } from '..';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+//fixme: change logic! see task!
