f1d9e303140a7fdc06fe39567ccd839df0ef5c5c
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-10-28 16:39:05 +0300

    added new functional

--- src/bin/games/brain-progression.js
+++ src/bin/games/brain-progression.js
@@ -1,57 +1,59 @@
 #!/usr/bin/env node
 import { whatsName, question, hiFunc } from '../index';
 
-hiFunc('Find the greatest common divisor of given numbers.');
+hiFunc('What number is missing in the progression?');
 
 const userName = whatsName();
 
-// const num1 = () => Math.floor(Math.random() * 100);
-// const num2 = () => Math.floor(Math.random() * 100);
-//
-// let count = 0;
-// let numer1 = 0;
-// let numer2 = 0;
+const num1 = () => Math.floor(Math.random() * 100);
+const num2 = () => Math.floor(Math.random() * 100);
 
-//https://uztest.ru/abstracts/?idabstract=1
+let count = 0;
+let numer1 = 0;
+let numer2 = 0;
 
-//fixme: !!! change -see task!
-// const findNOD = (a, b) => {
-//   let a1 = a;
-//   let b1 = b;
-//   while (a1 !== b1) {
-//     if (a1 > b1) {
-//       a1 -= b1;
-//     } else {
-//       b1 -= a1;
-//     }
-//   }
-//   return a1;
-// };
-//
-// const logic = (numQ, numAns) => {
-//   let result = false;
-//
-//   if (numQ === numAns) {
-//     console.log('Correct!');
-//     result = true;
-//   } else {
-//     console.log(`'${numAns}' is wrong answer ;(. Correct answer was '${numQ}'`);
-//     console.log(`Let's try again, ${userName}!`);
-//     result = false;
-//   }
-//   return result;
-// };
-//
-// while (count >= 0) {
-//   if (count === 3) {
-//     console.log(`Congratulations, ${userName}!`);
-//     process.exit(-1);
-//   }
-//   numer1 = num1();
-//   numer2 = num2();
-//
-//   if (!logic(findNOD(numer1, numer2), Number(question(`${numer1} ${numer2}`)))) {
-//     process.exit(-1);
-//   }
-//   count += 1;
-// }
+const genProg = (a1, d) => {
+  let prog = [];
+  let count = 0;
+  let str = '';
+  let indexX = Math.floor(Math.random() * 10);
+  while (count <10){
+    prog.push(a1+d*(count - 1));
+    if(count === indexX){
+      str +='..';
+    }else{
+      str += (a1+d(count - 1)).toString();
+    }
+  }
+  return [prog[indexX], str ];
+};
+
+const logic = (numQ, numAns) => {
+  let result = false;
+
+  if (numQ === numAns) {
+    console.log('Correct!');
+    result = true;
+  } else {
+    console.log(`'${numAns}' is wrong answer ;(. Correct answer was '${numQ}'`);
+    console.log(`Let's try again, ${userName}!`);
+    result = false;
+  }
+  return result;
+};
+
+while (count >= 0) {
+  if (count === 3) {
+    console.log(`Congratulations, ${userName}!`);
+    process.exit(-1);
+  }
+  numer1 = num1();
+  numer2 = num2();
+
+  [valX, valS] = genProg(numer1, numer2);
+
+  if (!logic(valX, Number(question(`${valS}`)))) {
+    process.exit(-1);
+  }
+  count += 1;
+}
