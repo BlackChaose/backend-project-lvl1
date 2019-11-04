9ac16bf4aee284440d85e351f581cc6521551425
nik <nik@0x4e494b.ru>
2019-11-05 02:34:14 +0300

    refactoring brain-even

--- src/bin/games/brain-even.js
+++ src/bin/games/brain-even.js
@@ -1,47 +1,22 @@
 #!/usr/bin/env node
-import { whatsName, question, hiFunc } from '../index';
+import run from '../engine';
 
-hiFunc('Answer "yes" if the number is even, otherwise answer "no".');
+const theme = 'Answer "yes" if the number is even, otherwise answer "no".';
+const dataSet = [];
 
-const userName = whatsName();
+for (let i = 0; i < 3; i += 1) {
+  dataSet[i] = Math.floor(Math.random() * 100);
+}
 
-const num = () => Math.floor(Math.random() * 100);
+console.log(dataSet);
 
-let count = 0;
-let numer = num();
-let ans = question(numer);
-
-const logic = (n, a) => {
+const checkEven = (n) => {
   if (n % 2 === 0) {
-    if (a === 'yes') {
-      count += 1;
-      console.log('Correct!');
-    } else {
-      count = 0;
-      console.log('"no" is wrong answer ;(. Correct answer was "yes".');
-      console.log(`let's try again, ${userName}!`);
-      process.exit(-1);
-    }
-  } else if (n % 2 !== 0) {
-    if (a === 'no') {
-      count += 1;
-      console.log('Correct!');
-    } else {
-      count = 0;
-      console.log('"yes" is wrong answer ;(. Correct answer was "no".');
-      console.log(`let's try again, ${userName}!`);
-      process.exit(-1);
-    }
+    return 'yes';
+  }
+  else {
+    return 'no';
   }
 };
 
-logic(numer, ans);
-while (count > 0) {
-  if (count === 3) {
-    console.log(`Congratulations, ${userName}!`);
-    process.exit(-1);
-  }
-  numer = num();
-  ans = question(numer);
-  logic(numer, ans);
-}
+run(theme, dataSet, checkEven);
