e6d600243ed4bf1056f8371710eec5df616f63c7
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-11-05 18:05:41 +0300

    fix bug & refactoring brain-calc; move functions from brain-calc to index.js

--- src/bin/games/brain-calc.js
+++ src/bin/games/brain-calc.js
@@ -1,59 +1,18 @@
 #!/usr/bin/env node
-import { whatsName, question, hiFunc } from '../index';
+import run from '../engine';
+import { randOp, getResOfCalc } from '../index';
 
-hiFunc('What is the result of the expression?');
+const theme = 'What is the result of the expression?';
+const dataSet = [];
 
-const userName = whatsName();
-
-const num1 = () => Math.floor(Math.random() * 100);
-const num2 = () => Math.floor(Math.random() * 100);
-
-let count = 0;
-let numer1 = 0;
-let numer2 = 0;
-let [funcOp, signOp] = [0, 0];
-
-const randOp = () => {
-  const bf = Math.floor(Math.random() * 10);
-  let res;
-  let sign = '';
-  if (bf <= 3) {
-    res = (a, b) => a + b;
-    sign = '+';
-  } else if (bf <= 7 && bf > 3) {
-    res = (a, b) => a - b;
-    sign = '-';
-  } else if (bf > 7) {
-    res = (a, b) => a * b;
-    sign = '*';
-  }
-  return [res, sign];
-};
-
-const logic = (numQ, numAns) => {
-  let result = false;
-
-  if (numQ === numAns) {
-    console.log('Correct!');
-    result = true;
-  } else {
-    console.log(`'${numAns}' is wrong answer ;(. Correct answer was '${numQ}'`);
-    console.log(`Let's try again, ${userName}!`);
-    result = false;
-  }
-  return result;
-};
-
-while (count >= 0) {
-  if (count === 3) {
-    console.log(`Congratulations, ${userName}!`);
-    process.exit(-1);
-  }
-  numer1 = num1();
-  numer2 = num2();
-  [funcOp, signOp] = randOp();
-  if (!logic(funcOp(numer1, numer2), Number(question(`${numer1} ${signOp} ${numer2}`)))) {
-    process.exit(-1);
-  }
-  count += 1;
+for (let i = 0; i < 3; i += 1) {
+  dataSet[i] = [Math.floor(Math.random() * 100),
+    Math.floor(Math.random() * 100),
+    randOp()];
 }
+
+run(theme,
+  dataSet,
+  dataSet.map((param) => `${param[0]} ${param[2]} ${param[1]}`),
+  getResOfCalc
+);
--- src/bin/index.js
+++ src/bin/index.js
@@ -1,19 +1,38 @@
-import readlineSync from 'readline-sync';
-
-const hiFunc = (comment = '') => {
-  console.log('Welcome to the Brain Games!');
-  if (comment !== '') {
-    console.log(comment);
+/**
+ * @nothing  operation sign generation function
+ * @returns {string}  '+', '-' or '*'
+ */
+const randOp = () => {
+  const bf = Math.floor(Math.random() * 10);
+  let sign = '';
+  switch (bf) {
+    case 1: sign = '+'; break;
+    case 2: sign = '+'; break;
+    case 3: sign = '+'; break;
+    case 4: sign = '-'; break;
+    case 5: sign = '-'; break;
+    case 6: sign = '-'; break;
+    case 7: sign = '-'; break;
+    default: sign = '*'; break;
   }
+  return sign;
+};
+/**
+ * @param param  array [a, b, 'sign']
+ * @returns {string}
+ */
+const getResOfCalc = (param) => {
+  const sign = param[2];
+  const a = param[0];
+  const b = param[1];
+  let res;
+  switch (sign) {
+    case '+': res = a + b; break;
+    case '-': res = a - b; break;
+    default: res = a * b; break;
+  }
+  return res.toString();
 };
 
-const whatsName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
-};
-const question = (num) => {
-  console.log(`Question ${num}`);
-  return readlineSync.question('Your answer: ');
-};
-export { whatsName, question, hiFunc };
+
+export { randOp, getResOfCalc };
