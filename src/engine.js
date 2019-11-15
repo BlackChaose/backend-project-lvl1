ab191504c16e5829353750fcf8c223e9c5958155
nik <nik@0x4e494b.ru>
2019-11-15 23:32:51 +0300

    change(run): delete const textQA, move text to console.log()'s

--- src/engine.js
+++ src/engine.js
@@ -1,38 +1,25 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const textQA = {
-  greeting: 'Welcome to the Brain Games!',
-  inputQuestion: 'May I have your name? ',
-  hello: 'Hello',
-  correctAnswer: 'Correct!',
-  wrongAnswer: ' is wrong answer ;(. Correct answer was ',
-  tryAgain: 'Let\'s try again,',
-  error: 'Error! Check datasets!',
-  congratulations: 'Congratulations',
-  question: 'Question: ',
-  yourAnswer: 'Your answer: ',
-};
-
 const run = (...params) => {
   const [dataset, cbFunc] = params;
   if (dataset === undefined
     || cbFunc === undefined) {
-    console.log(textQA.error);
+    console.log('Error!');
     process.exit(-1);
   }
-  console.log(textQA.greeting);
+  console.log('Welcome to the Brain Games!');
   console.log(dataset.theme);
-  const userName = readlineSync.question(textQA.inputQuestion);
-  console.log(`${textQA.hello} ${userName}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
   const logic = (numQ, numAns) => {
     let result = false;
     if (numQ === numAns) {
-      console.log(textQA.correctAnswer);
+      console.log('Correct!');
       result = true;
     } else {
-      console.log(`'${numAns}'${textQA.wrongAnswer}'${numQ}'`);
-      console.log(`${textQA.tryAgain} ${userName}!`);
+      console.log(`'${numAns}' is wrong answer ;(. Correct answer was '${numQ}'`);
+      console.log(`Let's try again, ${userName}!`);
       result = false;
     }
     return result;
@@ -40,11 +27,11 @@ const run = (...params) => {
 
   for (let i = 0; i <= 3; i += 1) {
     if (i === 3) {
-      console.log(`${textQA.congratulations} ${userName}!`);
+      console.log(`Congratulations ${userName}!`);
       process.exit(-1);
     }
-    console.log(`${textQA.question} ${dataset.questionsSet[i]}`);
-    const ans = readlineSync.question(textQA.yourAnswer);
+    console.log(`Question: ${dataset.questionsSet[i]}`);
+    const ans = readlineSync.question('Your answer: ');
     if (!logic(cbFunc(dataset.dataSet[i]), ans)) {
       process.exit(-1);
     }
